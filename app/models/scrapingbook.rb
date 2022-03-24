f34cf00e3211c26dfde5fd3296f8cbe217c3577c
T-Ito <you@example.com>
2022-03-24 19:02:49 +0900

    詳細画面修正

--- app/models/scrapingbook.rb
+++ app/models/scrapingbook.rb
@@ -11,20 +11,6 @@ class Scrapingbook
             else
                 blog = nil
             end
-            #target = "新・明解Javaで学ぶアルゴリズムとデータ構造"
-            target = "#{blog}#{book.title} #{book.author} #{book.publisher}" 
-            target_url = URI.encode_www_form(q: target)
-            url = "https://cgi.search.biglobe.ne.jp/cgi-bin/search-period?#{target_url}&num=10&lr=all&as_qdr=y"
-            html = URI.open(url).read
-            doc = Nokogiri::HTML.parse(html)
-            if doc.at_css('.hitCo01').present?
-                search_count = doc.at_css('.hitCo01').search('strong').text.to_i
-            else
-                search_count = 0
-            end
-            book.point = search_count
-            book.save
-            book.point
         #end
     end
 end
