7be0f352fb62037b4a81281156c50fac2b947997
T-Ito <you@example.com>
2022-03-24 19:29:19 +0900

    スクレーピング本番環境

--- app/models/scrapingbook.rb
+++ app/models/scrapingbook.rb
@@ -15,7 +15,7 @@ class Scrapingbook
             
             target_url = URI.encode_www_form(q: target)
             target
-            #url = "https://cgi.search.biglobe.ne.jp/cgi-bin/search-period?#{target_url}&num=10&lr=all&as_qdr=y"
+            url = "https://cgi.search.biglobe.ne.jp/cgi-bin/search-period?#{target_url}&num=10&lr=all&as_qdr=y"
         #end
     end
 end
